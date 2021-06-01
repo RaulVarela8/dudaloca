9b546c3eaf5aff825b16401b34fcf3c351fbb812
RaulVarela8 <84930523+RaulVarela8@users.noreply.github.com>
2021-06-01 14:38:39 -0300

    Add files via upload

---
+++ baleia.py
@@ -0,0 +1,17 @@
+nome="raul"
+anos=8
+print(nome+str(anos))
+
+nome="raul"
+anos=8
+print(nome+" "+str(anos)+"!")
+
+nome1="raul varela"
+print(nome1)
+print(nome1.upper())
+print(nome1.lower())
+print(nome1.title())
+
+print("raul\n varela")
+print("raul\tvarela")
+print("\\novo")
